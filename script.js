d433188c0eb5172e94d66828e2f6c0d5421f4de1
Kye Moon <jessyboustany2013@gmail.com>
2026-05-05 13:23:15 -0400

    alert

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
-const mySwiperOne = new Swiper ('.mySwiper', {
-direction: 'horizontal',
-  loop: true,
-  grabCursor: true,
-  effect: 'flip',
+const mySwiperOne = new Swiper('.mySwiper', {
+    direction: 'horizontal',
+    loop: true,
+    grabCursor: true,
+    effect: 'flip',
 
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
 });
 
+(function () {
+    const svgElement = document.querySelectorAll('#mail');
+    const statusElement = document.querySelectorAll('#gmail');
+    const textToCopy = "kyemoonovechka@gmail.com";
+
+    // Function to copy text to clipboard
+    async function copyText(text) {
+        if (navigator.clipboard && navigator.clipboard.writeText) {
+            // Modern async clipboard API
+            await navigator.clipboard.writeText(text);
+
+        }
+
+    }
+
+    // Attach click event to SVG
+    svgElement.addEventListener('click', () => {
+        copyText(textToCopy);
+        alert("✅ email copied! / ✅ courriel copié!"
+        );
+    });
+})();
